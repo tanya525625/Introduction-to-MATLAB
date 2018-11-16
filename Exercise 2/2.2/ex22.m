9c20fbb47fdae30baee2a252969e7ebb1fef8add
tanya525625 <tanya_postcrossing@mail.ru>
2018-11-16 20:35:31 +0300

    Ex2.1 was added

--- Exercise 2/2.2/ex22.m
+++ Exercise 2/2.2/ex22.m
@@ -6,6 +6,30 @@ ti = min(S.tt):0.1:max(S.tt);
 N = 2; 
 coeff1 = polyfit(S.tt, S.xx, N);
 newY1 = 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 for k=0:N
     newY1 = newY1 + coeff1(N-k+1) * ti.^k;
 end
